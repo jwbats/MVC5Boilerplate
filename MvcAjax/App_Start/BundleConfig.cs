5e1e3cfed47f6118ecefdf0e81e5bb533dfea8bd
jwbats <jwbats@gmail.com>
2015-07-05 16:05:35 +0200

    Initial commit.

---
+++ MvcAjax/App_Start/BundleConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace MvcAjax
+{
+	public class BundleConfig
+	{
+		public static void RegisterBundles(BundleCollection bundles)
+		{
+			bundles.Add(new StyleBundle("~/Content/css").Include(
+				"~/Content/bootstrap.css"
+			));
+
+			bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
+				"~/Scripts/jquery-1.9.1.js",
+				"~/Scripts/moment.js",
+				"~/Scripts/bootstrap.js",
+				"~/Scripts/bootstrap-datetimepicker.js"
+			));
+		}
+	}
+}
